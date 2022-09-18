448ac6faa26f0c2d73b38527f8476ced0ed2d57c
Mudar Hussain <mudar.shussain@gmail.com>
2022-09-18 20:08:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"mudar.shussain@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
